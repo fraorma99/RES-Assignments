49d3c58f622b925dc52de77c959a3d51be763859
raulalcazarr <s253268@dtu.dk>
2026-03-07 17:32:08 +0100

    definite step 2 raul

--- src/models.py
+++ src/models.py
@@ -127,6 +127,9 @@ class Step1MarketClearing:
         else:
             raise RuntimeError(f"Optimization of {self.model.ModelName} was not successful")
         
+# ========================
+# STEP 2: 24-HOUR DISPATCH
+# ========================
 
 class Step2InputData:
     """
@@ -163,8 +166,132 @@ class Step2InputData:
         self.hours = hours
         
         # Storage
-        self.P_ch = 400  # We consider that we cover approximately 35% of the demand with storage during peak load hours.
-        self.P_dis = 400
-        self.E = 800
-        self.eta_ch = 0.8
-        self.eta_dis = 0.85
+        self.P_ch = 500  # We consider that we cover approximately 12% of the demand with storage during peak load hours.
+        self.P_dis = 500
+        self.E = 2500
+        # As stated in ESR report, we consider a round-trip efficiency close to 0.8 which is a reasonable assumption for Pump Hydro Storage.
+        self.eta_ch = 0.9
+        self.eta_dis = 0.95
+
+class Step2MarketClearing:
+    """
+    Copper-plate market clearing over 24 hours with storage:
+    max sum_t [sum_n bid_tn * d_tn - sum_g c_g * pG_tg]
+    s.t. balance_t: sum_g pG_tg + sum_w pW_tw + pch_t - pdis_t = sum_n d_tn  ∀t
+         storage dyn: e_t = e_{t-1} + η_ch pch_t - pdis_t / η_dis  ∀t
+         bounds ∀t,g,w,n
+    """      
+    def __init__(self, input_data: Step2InputData, output_flag: int = 0):
+        self.data = input_data
+        self.variables = Expando()
+        self.constraints = Expando()
+        self.results = Expando()
+        self._build_model(output_flag=output_flag)
+
+    def _build_variables(self):
+        # Time index
+        T = self.data.hours
+        
+        # Generator dispatch
+        self.variables.pG = {(t, g): self.model.addVar(lb=0, ub=self.data.generator_pmax[g],
+                                                       name=f'pG_t{t}_g{g}')
+                             for t in T for g in self.data.GENERATORS}
+        
+        # Wind dispatch (bounded by availability)
+        self.variables.pW = {
+            (t, w): self.model.addVar(lb=0, ub=self.data.wind_avail_t[t][w],
+                                      name=f'pW_t{t}_w{w}')
+            for t in T for w in self.data.WINDS
+        }
+
+        # Elastic demand served
+        self.variables.d = {
+            (t, n): self.model.addVar(lb=0, ub=self.data.demand_pmax_t[t][n],
+                                      name=f'Demand served at bus {n} in hour {t}')
+            for t in T for n in self.data.LOAD_BUSES
+        }
+        
+        # Storage variables
+        self.variables.pch = {t: self.model.addVar(lb=0, ub=self.data.P_ch, name=f'pch_t{t}')
+                              for t in T}
+        self.variables.pdis = {t: self.model.addVar(lb=0, ub=self.data.P_dis, name=f'pdis_t{t}')
+                               for t in T}
+        self.variables.e = {t: self.model.addVar(lb=0, ub=self.data.E, name=f'e_t{t}')
+                            for t in T}
+        
+    def _build_constraints(self):
+        T = self.data.hours
+        
+        # System balance (copper plate)
+        # Note: in welfare maximization, the economic "market price" is typically -Pi of this constraint.
+        
+        self.constraints.balance = {}
+        for t in T:
+            
+            self.constraints.balance[t] = self.model.addLConstr(
+                gp.quicksum(self.variables.pG[(t, g)] for g in self.data.GENERATORS) +
+                gp.quicksum(self.variables.pW[(t, w)] for w in self.data.WINDS) -
+                self.variables.pch[t] + self.variables.pdis[t] -
+                gp.quicksum(self.variables.d[(t, n)] for n in self.data.LOAD_BUSES) == 0,
+                name=f'Balance_t{t}'
+            )
+        
+        # Storage dynamics (e_0 assume 0)
+        t0 = T[0]
+        self.constraints.dyn = {}
+        self.constraints.dyn[t0] = self.model.addLConstr(
+            self.variables.e[t0] == (self.data.eta_ch * self.variables.pch[t0]) -
+            (self.variables.pdis[t0] / self.data.eta_dis),
+            name='e_t0'
+        )
+        for t in T[1:]:
+            self.constraints.dyn[t] = self.model.addLConstr(
+                self.variables.e[t] == self.variables.e[t-1] +
+                self.data.eta_ch * self.variables.pch[t] -
+                self.variables.pdis[t] / self.data.eta_dis,
+                name=f'dyn_t{t}'
+            )
+
+    def _build_objective_function(self):
+        T = self.data.hours
+        welfare = (
+            gp.quicksum(self.data.demand_bid_t[t][n] * self.variables.d[(t, n)]
+                        for t in T for n in self.data.LOAD_BUSES) -
+            gp.quicksum(self.data.generator_cost[g] * self.variables.pG[(t, g)]
+                        for t in T for g in self.data.GENERATORS)
+            # wind/storage cost=0 omitted
+        )
+        self.model.setObjective(welfare, GRB.MAXIMIZE)
+
+    def _build_model(self, output_flag: int = 0):
+        self.model = gp.Model(name='Step 2 - Copper plate 24h with storage')
+        self.model.setParam('OutputFlag', output_flag)
+        self._build_variables()
+        self.model.update()
+        self._build_constraints()
+        self._build_objective_function()
+        self.model.update()
+
+    def _save_results(self):
+        T = self.data.hours
+        self.results.objective_value = self.model.ObjVal  # Total welfare 24h
+
+        # Dispatch por hora
+        self.results.pG = {(t, g): self.variables.pG[(t, g)].X for t in T for g in self.data.GENERATORS}
+        self.results.pW = {(t, w): self.variables.pW[(t, w)].X for t in T for w in self.data.WINDS}
+        self.results.d = {(t, n): self.variables.d[(t, n)].X for t in T for n in self.data.LOAD_BUSES}
+        self.results.pch = {t: self.variables.pch[t].X for t in T}
+        self.results.pdis = {t: self.variables.pdis[t].X for t in T}
+        self.results.e = {t: self.variables.e[t].X for t in T}
+
+        # Hourly prices: -Pi balance_t
+        self.results.market_prices_t = {t: -self.constraints.balance[t].Pi for t in T}
+        self.results.balance_duals = {t: self.constraints.balance[t].Pi for t in T}
+
+    def run(self):
+        self.model.optimize()
+        if self.model.status == GRB.OPTIMAL:
+            self._save_results()
+        else:
+            raise RuntimeError(f"Optimization of {self.model.ModelName} was not successful")
+  
--- src/postprocess.py
+++ src/postprocess.py
@@ -58,3 +58,112 @@ def step1_build_summary_tables(step1_results, input_data):
     }
 
     return df_gen, df_wind, df_dem, totals
+
+# ========================
+# STEP 2: 24-HOUR DISPATCH RESULTS
+# ========================
+
+def step2_build_summary_tables(step2_results, input_data):
+    """
+    Returns:
+    - df_gen: generator dispatch & profit (24h totals)
+    - df_wind: wind dispatch & profit (24h totals)
+    - df_dem: demand served & utility (24h totals)
+    - df_storage: storage operation & profit (hourly breakdown)
+    - df_prices: hourly market prices
+    - totals: dict with 24h aggregates
+    """
+    T = input_data.hours
+    prices_t = step2_results.market_prices_t
+    
+    # Generator dispatch & profit (aggregated 24h)
+
+    gen_rows = []
+    for g in input_data.GENERATORS:
+        total_pg = sum(step2_results.pG[(t, g)] for t in T)
+        cg = input_data.generator_cost[g]
+        # Profit = sum_t [price_t * pG_tg - cg * pG_tg]
+        profit = sum(prices_t[t] * step2_results.pG[(t, g)] - cg * step2_results.pG[(t, g)] for t in T)
+        gen_rows.append({
+            "generator": g,
+            "total_p_MWh": total_pg,  # MWh over 24h
+            "marginal_cost": cg,
+            "total_profit_24h": profit
+        })
+    df_gen = pd.DataFrame(gen_rows).sort_values("generator")
+    
+    # Wind dispatch & profit (aggregated 24h)
+
+    wind_rows = []
+    for w in input_data.WINDS:
+        total_pw = sum(step2_results.pW[(t, w)] for t in T)
+        # Profit = sum_t [price_t * pW_tw] (cost=0)
+        profit = sum(prices_t[t] * step2_results.pW[(t, w)] for t in T)
+        wind_rows.append({
+            "wind": w,
+            "total_p_MWh": total_pw,
+            "total_profit_24h": profit
+        })
+    df_wind = pd.DataFrame(wind_rows).sort_values("wind")
+    
+    # Demand served & utility (aggregated 24h)
+
+    dem_rows = []
+    for n in input_data.LOAD_BUSES:
+        total_dn = sum(step2_results.d[(t, n)] for t in T)
+        # Utility = sum_t [(bid_tn - price_t) * d_tn]
+        utility = sum((input_data.demand_bid_t[t][n] - prices_t[t]) * step2_results.d[(t, n)] for t in T)
+        dem_rows.append({
+            "bus": n,
+            "total_d_MWh": total_dn,
+            "total_utility_24h": utility
+        })
+    df_dem = pd.DataFrame(dem_rows).sort_values("bus")
+    
+    # Storage operation (hourly breakdown)
+    
+    storage_rows = []
+    for t in T:
+        pch = step2_results.pch[t]
+        pdis = step2_results.pdis[t]
+        e = step2_results.e[t]
+        # Profit = price_t * (pdis_t - pch_t)  (bids=0, arbitrage)
+        profit_t = prices_t[t] * (pdis - pch)
+        storage_rows.append({
+            "hour": t,
+            "pch_MW": pch,
+            "pdis_MW": pdis,
+            "e_MWh": e,
+            "price": prices_t[t],
+            "profit_t": profit_t
+        })
+    df_storage = pd.DataFrame(storage_rows)
+    
+    # Hourly prices
+    df_prices = pd.DataFrame([{"hour": t, "price": prices_t[t]} for t in T])
+
+    # Total operating cost (24h)
+    
+    total_operating_cost = sum(
+        input_data.generator_cost[g] * step2_results.pG[(t, g)]
+        for t in T for g in input_data.GENERATORS
+    )
+    
+    # Aggregated totals
+    
+    totals = {
+        "objective_welfare_24h": step2_results.objective_value,
+        "total_operating_cost_24h": total_operating_cost,
+        "total_gen_MWh": df_gen["total_p_MWh"].sum(),
+        "total_wind_MWh": df_wind["total_p_MWh"].sum(),
+        "total_demand_served_MWh": df_dem["total_d_MWh"].sum(),
+        "total_gen_profit_24h": df_gen["total_profit_24h"].sum(),
+        "total_wind_profit_24h": df_wind["total_profit_24h"].sum(),
+        "total_demand_utility_24h": df_dem["total_utility_24h"].sum(),
+        "total_storage_profit_24h": df_storage["profit_t"].sum(),
+        "avg_price": df_prices["price"].mean(),
+        "max_price": df_prices["price"].max(),
+        "min_price": df_prices["price"].min(),
+    }
+
+    return df_gen, df_wind, df_dem, df_storage, df_prices, totals
